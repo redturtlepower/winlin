764fd1514e35f25832bf493a67ba73c618393667
Jenkins <jenkins@Thomass-Mac-mini.local>
2018-11-24 00:01:40 +0100

    do not clone if there is a local repo

--- ubuntu/provision.sh
+++ ubuntu/provision.sh
@@ -1,1 +1,6 @@
-git clone "https://github.com/redturtlepower/qt-installer.git" && cd qt-installer && chmod +x setup.sh && bash setup.sh
+if [ "$(ls -a qt-installer)" ]; then
+	echo "The directory 'qt-installer' is not empty. It is assumed that Qt has been installed already."
+else
+	echo "The directory 'qt-installer' is empty. Cloning repo and installing Qt..."
+	git clone "https://github.com/redturtlepower/qt-installer.git" && cd qt-installer && chmod +x setup.sh && bash setup.sh
+fi
