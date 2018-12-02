660f2cb6803cb5e89762b013c457283adabbeb7e
Jenkins <jenkins@Thomass-Mac-mini.local>
2018-12-02 14:39:04 +0100

    corrected base image

--- android/provision/provision.sh
+++ android/provision/provision.sh
@@ -9,16 +9,5 @@ else
 fi
 
 # Install Android NDK + SDK
-
-# docker-build --no-cache -t ubuntu-qt 
-# docker run ubuntu-qt # to run startup cmd (install qt)
-# docker commit -t ubuntu-qt
-# docker exec -it ubuntu-qt-buildenv /bin/bash
-
-
-# Install Qt development dependencies
-# RUN git clone "https://github.com/redturtlepower/qt-installer.git" && cd qt-installer && chmod +x setup.sh && bash setup.sh
-
-# Install Android development dependencies (Android SKD + NDK, Java JDK)
 # https://dl.google.com/android/repository/android-ndk-r18b-linux-x86_64.zip 
 # RUN curl -sL https://dl.google.com/android/repository/android-ndk-r18b-linux-x86_64.zip | tar -xJC  ~/.ndk
--- ubuntu/provision.sh
+++ ubuntu/provision.sh
@@ -1,3 +1,4 @@
+# Install Qt
 if [ "$(ls -a qt-installer)" ]; then
 	echo "The directory 'qt-installer' is not empty. It is assumed that Qt has been installed already."
 else
@@ -6,8 +7,3 @@ else
 	# Remove compiler android
 	rm -rf ~/Qt5.11.2/5.11.2/android_armv7
 fi
-
-# docker-build --no-cache -t ubuntu-qt 
-# docker run ubuntu-qt # to run startup cmd (install qt)
-# docker commit -t ubuntu-qt
-# docker exec -it ubuntu-qt-buildenv /bin/bash
